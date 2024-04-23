5bea3fe92fb74751cc2be7f853f0ceab1daf8e60
quguiyou <quguiyou1076@qq.com>
2024-04-23 21:29:57 +0800

    模拟器框架

--- src/rvemu.h
+++ src/rvemu.h
@@ -13,10 +13,12 @@
 #include <unistd.h>
 
 #include "elfdef.h"
+#include "reg.h"
 #include "types.h"
 
 #define fatal(msg) \
   (fprintf(stderr, "fatal: %s:%d %s\n", __FILE__, __LINE__, msg), exit(1))
+#define unreachable() (fatal("unreachable"), __builtin_unreachable())
 
 #define ROUNDDOWN(x, k) ((x) & -(k))
 #define ROUNDUP(x, k) (((x) + (k)-1) & -(k))
@@ -27,6 +29,21 @@
 
 #define TO_HOST(addr) (addr + GUEST_MEMORY_OFFSET)
 #define TO_GUEST(addr) (addr - GUEST_MEMORY_OFFSET)
+
+enum insn_type_t {
+  insn_addi,
+  num_insns,
+};
+
+typedef struct {
+  i8 rd;
+  i8 rs1;
+  i8 rs2;
+  i32 imm;
+  enum insn_type_t type;
+  bool rvc;
+  bool cont;
+} insn_t;
 
 /**
  * mmu.c
@@ -43,7 +60,15 @@ void mmu_load_elf(mmu_t *mmu, int fd);
 /**
  * state.c
  */
+enum exit_reason_t {
+  none,
+  direct_branch,
+  indirect_branch,
+  ecall,
+};
+
 typedef struct {
+  enum exit_reason_t exit_reason;
   u64 gp_regs[32];
   u64 pc;
 } state_t;
@@ -57,3 +82,15 @@ typedef struct {
 } machine_t;
 
 void machine_load_program(machine_t *m, char *prog);
+
+enum exit_reason_t machine_step(machine_t *m);
+
+/**
+ * interp.c
+ */
+void exec_block_interp(state_t *state);
+
+/**
+ * decode.c
+ */
+void insn_decode(insn_t *insn, u32 data);
